239162b99235350e72af36d61a8efa5a74bc7b63
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2022-05-16 15:37:38 -0400

    done added user

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+const user = {
+    FirstName: 'antonio',
+    LastName: 'ruiz',
+    email: "antonio.Ruiz@gmail.com",
+    UserName: "antRuiz"
+}
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+    res.status(200), res.send('Antonio Ruiz' + 'status code is: ' + res.statusCode)
 
 })
 
 app.get('/Home', (req, res) => {
-    res.send('and another hello from home page.... well sever')
+    res.status(203), res.send('home page' + 'status code is: ' + res.statusCode)
+
+})
+app.get('/user', (req, res) => {
+
+    res.send(user.FirstName + user.LastName)
+})
+
+app.get('*', (req, res) => {
+    res.status(404), res.send('catch all route' + 'status code is: ' + res.statusCode)
 })
 
 app.listen(port, () => {
